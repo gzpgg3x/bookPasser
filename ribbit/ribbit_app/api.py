fcc47a6d4cdce750794150510fa159faa2411fd0
Forrest Pan <gzpgg3x@yahoo.com>
2013-05-18 22:11:28 -0400

    demoVersion1.01beforeBypassingDVDeBook

--- ribbit/ribbit_app/api.py
+++ ribbit/ribbit_app/api.py
@@ -36,7 +36,7 @@ def new_shout(request):
     brcount=0
     br=range(100)
     bradd=range(100)
-    for row in cur.execute("SELECT * FROM ribbit_app_branch"): #order by visit_count DESC"):   
+    for row in cur.execute("SELECT * FROM ribbit_app_branch"): 
         br[brcount] = row[1]
         bradd[brcount] = row[0]
         print brcount
@@ -55,26 +55,13 @@ def new_shout(request):
     # keywords = keywords.lower()
 
     # START TO READ FROM BOOKPASSER, WRITE INTO SHOUT TABLE THE BOOK I CHECKED-IN
-    # con = lite.connect("C:/Users/fpan/PY-Programs/bookPasser/ribbit/ribbit/database.db")
-    # cur = con.cursor()
     count=0
     response = []
     chkincount=0
     chkin=range(15)
     chkinloc=range(15)
     Shout.objects.all().delete()
-    for row in cur.execute("SELECT * FROM ribbit_app_bookpasser"): #order by visit_count DESC"):   
-        # chkin[chkincount] = row[2]
-        # chkinloc[chkincount] = row[3]
-        # chkincount=chkincount+1 
-        # if keywords == chkin[chkincount]:                      
-        #     count = chkincount
-        #     print chkin[chkincount]
-        #     print chkinloc[chkincount]
-        #     print chkincount
-        #     print chkin[chkincount]
-        #     print chkinloc[chkincount]
-        #     count=count+1    
+    for row in cur.execute("SELECT * FROM ribbit_app_bookpasser"):    
         if keywords == row[2]:
             chkin[chkincount] = row[2]
             chkinloc[chkincount] = row[3]
@@ -105,8 +92,6 @@ def new_shout(request):
             'count': count
         })
 
-
-
     # START TO READ FROM SHOUT TABLE
     # Shout.objects.all().delete()
 
@@ -120,9 +105,6 @@ def new_shout(request):
     d=author
     dd=message
     a=0
-    # count=0
-    # response = []
-    # ddd = 'http://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text='+keywords+'&sort=Date&formType=Form8K&isAdv=true&stemming=true&numResults=100&fromDate=' + d + '&toDate=' + dd + '&numResults=100' 
     ddd = 'http://nypl.bibliocommons.com/search?t=title&q=' + kw + '&commit=Search&searchOpt=catalogue' 
     responser=get_url_content(ddd)
     # print responser
@@ -134,13 +116,9 @@ def new_shout(request):
             if bkkw in y:      
                 print y
                 bkcode = y[11:25]
-
                 bkcode = 'http://nypl.bibliocommons.com/item/show_circulation/' + bkcode
                 # print bkcode 
                 responser=get_url_content(bkcode)
-                # beg = responser.index("Available to borrow",1)
-                # end = responser.index("Available to borrow",beg)
-                # responser = responser[beg:end]
                 clean_text = nltk.clean_html(responser)
                 beg = clean_text.index("Available to borrow",1)
                 end = clean_text.index("Not available at this time",beg)
@@ -156,8 +134,6 @@ def new_shout(request):
                         count = count + 1
                         print bradd[r]
                 # print clean_text
-
-
                         shout = Shout.objects.create(lat=lat,lng=lng,author=author,message=message,book=book,address=address,branchname=branchname,count=count)
 
                         response.append({
@@ -173,67 +149,7 @@ def new_shout(request):
                             'count': count
                         })
 
-
-
                 break
-
-
-
-    # xLast=""
-    # zz=""
-    # x=" "
-    # e=0
-    # for link in BeautifulSoup(responser, parseOnlyThese=SoupStrainer('a')):
-    #     if link.has_key('href'):
-    #         y = link['href']
-    #         if 'javascript:opennew(' in y: # and xLast is not x:
-    #             b = y.index("'",21)
-    #             z = y[20:b]
-    #             c = y.index("'", b+4)
-    #             x = y[b+3:c]
-    #             if not (xLast == x):# and zz is z[1:45]:
-    #                 text = get_url_content(z)
-    #                 zzz=dehtml(text)
-    #                 xx=x[0:40]                
-    #                 try:
-    #                     e = zzz.index(kw ,1) #zzz.index("Item 5.02",1)
-    #                 except:
-    #                     e = 1
-    #                 newzzz = zzz[e:e+250]
-    #                 zz=z[1:45]
-    #                 xLast=x
-    #                 author=xx
-    #                 message=z
-    #                 cikidxbeg = y.index("http://www.sec.gov/Archives/edgar/data/",1) + 40
-    #                 cikidxend = y.index("/", cikidxbeg)
-    #                 cik = y[cikidxbeg-1:cikidxend]
-    #                 cikText = get_url_content('http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=' + cik)
-    #                 mailerLocA = cikText.index("<div class=\"mailer\">",1)
-    #                 mailerLocB = cikText.index("<div class=\"mailer\">", mailerLocA + 20)
-    #                 mailerText = cikText[mailerLocA:mailerLocB + 20]
-    #                 address = dehtml(mailerText)
-    #                 postal_code = re.match('^.*?(\d+)$', address)
-    #                 if postal_code!=None:
-    #                     zip = address[-5:len(address)]
-
-    #                     if zip[-5:-4] == "-":
-    #                         zip = address[-10:-6]
-
-    #                     if zip >= "07000" and zip <="09000":
-    #                         a = a + 1
-    #                         address = address[16:len(address)]
-    #                         shout = Shout.objects.create(lat=lat,lng=lng,author=author,message=message)
-
-    #                         response.append({
-    #                             'date_created': shout.date_created.strftime("%b %d at %I:%M:%S%p"),
-    #                             'lat': str(shout.lat),
-    #                             'lng': str(shout.lng),
-    #                             'author': author,
-    #                             'message': message,
-    #                             'zipcode': zip,
-    #                             'address': address,
-    #                             'count': a
-    #                         })
 
     return HttpResponse(json.dumps(response))
 
